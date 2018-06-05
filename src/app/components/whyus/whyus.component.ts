df6b64813ecafadc698bbf8f6fcd2c1cc8c46e7b
Kathrin <kathrin@kathrins-air.codefactoryvienna.local>
2018-06-05 09:08:04 +0200

    update whyus files

--- src/app/components/whyus/whyus.component.ts
+++ src/app/components/whyus/whyus.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-whyus',
+  selector: 'whyus',
   templateUrl: './whyus.component.html',
   styleUrls: ['./whyus.component.css']
 })
