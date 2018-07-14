2f695d1ee47ad3427ec7e06037e7723454d555dc
Tim Almamedov <tim.almamedov@gmail.com>
2018-07-14 14:40:45 +0200

    changed asset path to absolute, for deplyoment purposes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyBY-hLrSBar0NykRex6dLqBB31pZee0wFQ",
+  authDomain: "jugend-college.firebaseapp.com",
+  databaseURL: "https://jugend-college.firebaseio.com",
+  projectId: "jugend-college",
+  storageBucket: "jugend-college.appspot.com",
+  messagingSenderId: "9590576031"
 };
